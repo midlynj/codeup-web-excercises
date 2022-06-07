a0bda6fb81f207c5c315a5524b50efea685c3f03
midlynj <jmidlyn@gmail.com>
2022-06-07 12:18:49 -0500

    more loop exercises

---
+++ js/break_and_continue.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let x = prompt("Enter an odd number between 1 and 50.")
+
+while (x % 2 === 0) {
+    x = prompt("Enter an odd number between 1 and 50.")
+    if (x % 2 !== 0) {
+        break;
+    }
+}for (var i = 1; i < 50; i+=2) {
+    if ( i == x) {
+        console.log(i + " is skipped");
+        continue;
+    } console.log("Here is an odd number " + i);
+}
