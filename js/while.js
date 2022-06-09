4e4d93bd8caf60fc016b28a947d283622bb13679
midlynj <jmidlyn@gmail.com>
2022-06-08 22:19:51 -0500

    planets array exercise

--- js/while.js
+++ js/while.js
@@ -7,18 +7,16 @@ while (i <= 65536) {
 }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
+console.log("I have " + allCones + " cones")
 
 do {
     let conesBought = Math.floor(Math.random() * 5) + 1;
-
-    if (conesBought >= allCones) {
-        allCones = allCones - conesBought
-
-        console.log(conesBought + "cones sold. I have " + allCones)
+    if (conesBought < allCones) {
+        allCones = allCones - conesBought;
+        console.log(conesBought + " cones sold. I have " + allCones + " cones left");
     } else {
-        console.log("I dont have enough cones")
+        console.log("I dont have enough cones");
+        break;
     }
-
-
-} while (allCones !== 0)
-console.log("All cones are sold")
+} while (allCones == 0)
+console.log("all cones are sold");
